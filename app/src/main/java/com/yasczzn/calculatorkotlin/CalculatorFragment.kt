9ad6f3c9aed7faa21eb0b014c4af053804e4cf2d
yaswu <putriyasminarahmaz@gmail.com>
2022-10-07 16:51:05 +0700

    fourth commit
    Edit 11 files, add 1 file, delete 1 file

--- app/src/main/java/com/yasczzn/calculatorkotlin/CalculatorFragment.kt
+++ app/src/main/java/com/yasczzn/calculatorkotlin/CalculatorFragment.kt
@@ -1,19 +1,28 @@
 package com.example.calculatorkotlin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.calculatorkotlin.databinding.FragmentCalculatorBinding
 
 class CalculatorFragment : Fragment() {
+
+    private var _binding: FragmentCalculatorBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
     }
+
 
 }
