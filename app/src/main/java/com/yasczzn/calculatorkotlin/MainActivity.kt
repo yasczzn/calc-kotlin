7e3cdf9a93c8f669d9c8a1a7773a8e6e3820384b
yaswu <putriyasminarahmaz@gmail.com>
2022-10-07 17:49:39 +0700

    sixth commit
    Edit 15 files, delete 1 file

--- app/src/main/java/com/yasczzn/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/yasczzn/calculatorkotlin/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.example.calculatorkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.viewpager.widget.ViewPager
 import com.example.calculatorkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     
     private lateinit var binding: ActivityMainBinding
+    private val mViewPager: ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +21,20 @@ class MainActivity : AppCompatActivity() {
         binding.btnChange.setOnClickListener(View.OnClickListener {
             findNavController(R.id.calc_frame).navigate(R.id.action_calculatorFragment_to_convertorFragment)
         })
+
+        mViewPager!!.addOnPageChangeListener(ViewPager . OnPageChangeListener () {
+            fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        supportActionBar?.setTitle(R.string.calc_page)
+                    }
+                    1 -> {
+                        supportActionBar?.setTitle(R.string.conv_page)
+                    }
+                    else -> {}
+                }
+            }
     }
 }
