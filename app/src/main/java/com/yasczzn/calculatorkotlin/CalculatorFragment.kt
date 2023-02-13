5c29148b5378cab4ec61ce33e82d2d5b4d081de8
yaswu <putriyasminarahmaz@gmail.com>
2023-02-13 17:17:56 +0700

    commit 130223
    Edit 7 files

--- app/src/main/java/com/yasczzn/calculatorkotlin/CalculatorFragment.kt
+++ app/src/main/java/com/yasczzn/calculatorkotlin/CalculatorFragment.kt
@@ -24,8 +24,6 @@ class CalculatorFragment : Fragment() {
         _binding = FragmentCalculatorBinding.inflate(layoutInflater)
         return binding.root
 
-        binding.
-
     }
 
     fun numberAct(view: View) {
